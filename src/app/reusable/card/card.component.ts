7832faf77976e1d9e7cbab05929d5f89c66db49a
Keo Ny Anann <keonyanann@Keos-MacBook-Air.local>
2020-11-18 07:55:52 +0700

    added images and changes to card

--- src/app/reusable/card/card.component.ts
+++ src/app/reusable/card/card.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  card = {
+    title: 'Title of the design',
+    color: 'NAVY',
+    frontImage : 'https://source.unsplash.com/random/300x400'
+  }
   constructor() { }
 
   ngOnInit(): void {
