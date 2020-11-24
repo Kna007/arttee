b5c175f2ae016c706026c0836973b662f398c732
KNA007 <keonyanann@gmail.com>
2020-11-24 13:47:05 +0700

    made color selection customize page

--- src/app/customize/customize.component.ts
+++ src/app/customize/customize.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fabric } from 'fabric';
 @Component({
   selector: 'app-customize',
   templateUrl: './customize.component.html',
   styleUrls: ['./customize.component.scss']
 })
-export class CustomizeComponent implements OnInit {
-
+export class CustomizeComponent implements AfterViewInit {
+  @ViewChild('shirtLayer') shirtLayer: ElementRef;
   constructor() { }
-
-  ngOnInit(): void {
+  dropdownClicked = false;
+  selectedColor = 'BLACK'
+  ngAfterViewInit(): void {
+    this.shirtLayer.nativeElement.width = window.innerWidth * 0.8;
+    this.shirtLayer.nativeElement.height = window.innerWidth * 0.8* 1.2;
+    let shirtLayerContext = this.shirtLayer.nativeElement.getContext('2d');
+    let shirtImage = new Image();
+    shirtImage.src = '/assets/BLACK-BACK.png';
+    shirtImage.onload = () =>{
+      shirtLayerContext.drawImage(shirtImage,0,0,shirtImage.width,shirtImage.height);
+    }
   }
 
 }
